202a4669940c60847ac857e54ccc6f157a6d5012
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-02-11 11:14:09 +0000

    Basic css for ViewArticles completed

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Articles.css";
 
-const Articles = props => {
+const Articles = ({ articles }) => {
+  console.log(articles);
   return (
     <div>
-      {props.articles.map(article => {
+      {articles.map(article => {
         return (
-          <Link key={article.article_id} to={`/articles/${article.article_id}`}>
-            <li>{article.title}</li>
-          </Link>
+          <ul className="articles__ul">
+            <li className="articles__author">Author: {article.author}</li>
+            <li className="articles__title">{article.title}</li>
+            <Link key={article.article_id} to={`/articles/${article.article_id}`}>
+              {" "}
+              <button className="article__btn">Read more</button>{" "}
+            </Link>
+          </ul>
         );
       })}
     </div>
