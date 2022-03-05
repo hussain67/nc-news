f27bff74d48ce30a11abaf12d68864a3111e92b7
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-03-05 15:15:46 +0000

    loading spinner added

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,9 @@
 import React from "react";
-//import "../styles/Loading.css";
+import "../styles/Loading.css";
 const Loading = () => {
   return (
     <div className="center">
-      <div>Loading...</div>
+      <div className="loading"></div>
     </div>
   );
 };
