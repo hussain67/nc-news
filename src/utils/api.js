67ed870afa3db8274f5d2474853be722b499473e
Shahid Hussain <shahidhussain@Shahids-Air.broadband>
2022-12-23 02:07:28 +0000

    baseUrl changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,74 +1,75 @@
 import axios from "axios";
 
 const newsApi = axios.create({
-  baseURL: "https://be-nc-news-shahid.herokuapp.com/api"
+	baseURL: "https://be-nc-news-e5v6.onrender.com"
+	//baseURL: "http://localhost:9090/api"
 });
 
 export const getArticles = () => {
-  return newsApi
-    .get(`/articles`)
-    .then(res => {
-      return res.data;
-    })
-    .catch(err => {
-      console.log(err.message);
-      return err.message;
-    });
+	return newsApi
+		.get(`/articles`)
+		.then(res => {
+			return res.data;
+		})
+		.catch(err => {
+			console.log(err.message);
+			return err.message;
+		});
 };
 export const getArticlesById = id => {
-  return newsApi
-    .get(`/articles/${id}`)
-    .then(res => {
-      return res.data;
-    })
-    .catch(err => {
-      console.log(err.message);
-      return err.message;
-    });
+	return newsApi
+		.get(`/articles/${id}`)
+		.then(res => {
+			return res.data;
+		})
+		.catch(err => {
+			console.log(err.message);
+			return err.message;
+		});
 };
 export const getCommentsByArticleId = id => {
-  return newsApi.get(`/articles/${id}/comments`).then(res => {
-    return res.data;
-  });
+	return newsApi.get(`/articles/${id}/comments`).then(res => {
+		return res.data;
+	});
 };
 export const getArticlesByTopic = topic => {
-  return newsApi.get(`/articles?topic=${topic}`).then(res => {
-    return res.data;
-  });
+	return newsApi.get(`/articles?topic=${topic}`).then(res => {
+		return res.data;
+	});
 };
 export const getTopics = () => {
-  return newsApi.get("/topics").then(res => {
-    return res.data;
-  });
+	return newsApi.get("/topics").then(res => {
+		return res.data;
+	});
 };
 export const getArticlesByQuery = query => {
-  return newsApi.get(`/articles?sort_by=${query}`).then(res => {
-    return res.data;
-  });
+	return newsApi.get(`/articles?sort_by=${query}`).then(res => {
+		return res.data;
+	});
 };
 export const postComment = (id, username, body) => {
-  return newsApi
-    .post(`/articles/${id}/comments`, {
-      username,
-      body
-    })
-    .then(res => {
-      return res;
-    });
+	return newsApi
+		.post(`/articles/${id}/comments`, {
+			username,
+			body
+		})
+		.then(res => {
+			return res;
+		});
 };
 export const deleteComment = comment_id => {
-  return newsApi.delete(`/comments/${comment_id}`).then(res => {
-    return res;
-  });
+	return newsApi.delete(`/comments/${comment_id}`).then(res => {
+		return res;
+	});
 };
 export const countVote = (id, count, element) => {
-  if (element === "article") {
-    return newsApi.patch(`/articles/${id}`, { inc_votes: count }).then(res => {
-      return res;
-    });
-  } else if (element === "comment") {
-    return newsApi.patch(`/comments/${id}`, { inc_votes: count }).then(res => {
-      return res;
-    });
-  }
+	if (element === "article") {
+		return newsApi.patch(`/articles/${id}`, { inc_votes: count }).then(res => {
+			return res;
+		});
+	} else if (element === "comment") {
+		return newsApi.patch(`/comments/${id}`, { inc_votes: count }).then(res => {
+			return res;
+		});
+	}
 };
